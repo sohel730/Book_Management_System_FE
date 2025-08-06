c728101c3a054b9ed99246324109758982a237c8
Sohel Momin <momin8805887375@gmail.com>
2025-08-06 11:52:49 +0530

    changes regarding registration page pushed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Ragistration from './Components/Ragistrationpage/Ragistration'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
      <h1>sohel momin is so brave guy.</h1>
+     < Ragistration/>
     </>
   )
 }
---
+++ src/Components/Ragistrationpage/Ragistration.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Ragistration.css'
+
+
+const Ragistration=()=> {
+  return (
+      <div className='b'>Ragistration</div>
+      
+
+
+  )
+}
+
+export default Ragistration
